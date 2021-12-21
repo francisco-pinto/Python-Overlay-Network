a14af6824136910ddbda75edd2c53642aa2776ca
Francisco <ffcpinto2@gmail.com>
2021-12-21 12:51:19 +0000

    Correcting bug

--- Servidor.py
+++ Servidor.py
@@ -9,45 +9,10 @@ from xml.dom import minidom
 from VideoStream import VideoStream
 from RtpPacket import RtpPacket
 
+from Graph import Graph
 from collections import defaultdict
 
-class Graph(object):
-    def __init__(self, nodes, init_graph):
-        self.nodes = nodes
-        self.graph = self.construct_graph(nodes, init_graph)
-        
-    def construct_graph(self, nodes, init_graph):
-        '''
-        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
-        '''
-        graph = {}
-        for node in nodes:
-            graph[node] = {}
-        
-        graph.update(init_graph)
-        
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-                    
-        return graph
-    
-    def get_nodes(self):
-        "Returns the nodes of the graph."
-        return self.nodes
-    
-    def get_outgoing_edges(self, node):
-        "Returns the neighbors of a node."
-        connections = []
-        for out_node in self.nodes:
-            if self.graph[node].get(out_node, False) != False:
-                connections.append(out_node)
-        return connections
-    
-    def value(self, node1, node2):
-        "Returns the value of an edge between two nodes."
-        return self.graph[node1][node2]
+
 	
 class Interface:
 	ip=0
@@ -56,6 +21,7 @@ class Interface:
 	def __init__(self, ip, port):
 		self.ip=ip
 		self.port=port
+
 class Connection:
 	fromNode=0
 	fromIP=0
@@ -67,6 +33,7 @@ class Connection:
 		self.fromIP=fromIP
 		self.toNode=toNode
 		self.toIP=toIP
+
 class Node:
 	id=0
 	interfaces=[]
@@ -86,6 +53,7 @@ class Node:
 	
 	def addInterface(self, ip, port):
 		self.interfaces.append(Interface(ip, port))
+
 class Servidor:	
 
 	clientInfo = {}
@@ -99,54 +67,6 @@ class Servidor:
 		maintenance=threading.Thread(target=self.TopologyMaintenance, args=())
 		maintenance.start()
 		print("inicio")
-
-	def dijkstra_algorithm(self, graph, start_node):
-		unvisited_nodes = list(graph.get_nodes())
-		shortest_path = {}
-		previous_nodes = {}
-
-		# We'll use max_value to initialize the "infinity" value of the unvisited nodes   
-		max_value = sys.maxsize
-
-		for node in unvisited_nodes:
-			shortest_path[node] = max_value
-		# However, we initialize the starting node's value with 0   
-		shortest_path[start_node] = 0
-
-		while unvisited_nodes:
-			current_min_node = None
-			
-			for node in unvisited_nodes: # Iterate over the nodes
-				if current_min_node == None:
-					current_min_node = node
-				elif shortest_path[node] < shortest_path[current_min_node]:
-					current_min_node = node
-					
-			# The code block below retrieves the current node's neighbors and updates their distances
-			neighbors = graph.get_outgoing_edges(current_min_node)
-			
-			for neighbor in neighbors:
-				tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-				
-				#print(tentative_value)
-				
-				if tentative_value < shortest_path[neighbor]:
-					shortest_path[neighbor] = tentative_value
-					# We also update the best path to the current node
-					previous_nodes[neighbor] = current_min_node
-			
-			print(current_min_node)
-			unvisited_nodes.remove(current_min_node)
-		
-
-		return previous_nodes, shortest_path
-
-
-
-
-
-
-
 
 
 	def CalculateShortestPath(self):
@@ -179,25 +99,9 @@ class Servidor:
 		
 		graph = Graph(nodeId, init_graph)
 
-		previous_nodes, shortest_path = self.dijkstra_algorithm(graph, "n1")
+		previous_nodes, shortest_path = graph.dijkstra_algorithm("n1")
 
-		self.print_result(previous_nodes, shortest_path, "n1", "n2")
-
-
-
-	def print_result(self, previous_nodes, shortest_path, start_node, target_node):
-		path = []
-		node = target_node
-
-		while node != start_node:
-			path.append(node)
-			node = previous_nodes[node]
-	
-		# Add the start node manually
-		path.append(start_node)
-		
-		print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
-		print(" -> ".join(reversed(path)))
+		graph.print_result(previous_nodes, shortest_path, "n1", "n5")
 
 	def GetNetworkTopology(self):
 		file = minidom.parse('Topologia.xml')
