57a606b6146d1a0fd166dc78458a439147552be6
Francisco <ffcpinto2@gmail.com>
2021-12-21 12:43:01 +0000

    Dijkstra algoritm with print path

--- Servidor.py
+++ Servidor.py
@@ -9,25 +9,46 @@ from xml.dom import minidom
 from VideoStream import VideoStream
 from RtpPacket import RtpPacket
 
-from queue import PriorityQueue
+from collections import defaultdict
 
-class Graph:
-	def __init__(self, num_of_nodes):
-		self.v = num_of_nodes
-		self.edges = [[-1 for i in range(num_of_nodes)] for j in range(num_of_nodes)]
-		print(self.edges)
-		self.visited = []
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+        
+    def construct_graph(self, nodes, init_graph):
+        '''
+        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+        
+        graph.update(init_graph)
+        
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+                    
+        return graph
+    
+    def get_nodes(self):
+        "Returns the nodes of the graph."
+        return self.nodes
+    
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+    
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        return self.graph[node1][node2]
 	
-	#1 if connection exist
-	#-1 if connection don't exist
-	def	add_edge(self, u, v, weight):
-		#print("A adicionar edge")
-		try:
-			self.edges[u][v] = weight
-			self.edges[v][u] = weight	
-		except:
-			"""Link disconnected"""
-			#print("Link disconnected")
 class Interface:
 	ip=0
 	port=0
@@ -78,32 +99,106 @@ class Servidor:
 		maintenance=threading.Thread(target=self.TopologyMaintenance, args=())
 		maintenance.start()
 		print("inicio")
-		
-	def CalculateShortestPath(self):
-		graphLen=0
-		print("Caminho mais curto")
-		for node in self.nodes:
-			if node.online==1:
-				graphLen=graphLen+1
 
-		g=Graph(graphLen)
+	def dijkstra_algorithm(self, graph, start_node):
+		unvisited_nodes = list(graph.get_nodes())
+		shortest_path = {}
+		previous_nodes = {}
+
+		# We'll use max_value to initialize the "infinity" value of the unvisited nodes   
+		max_value = sys.maxsize
+
+		for node in unvisited_nodes:
+			shortest_path[node] = max_value
+		# However, we initialize the starting node's value with 0   
+		shortest_path[start_node] = 0
+
+		while unvisited_nodes:
+			current_min_node = None
+			
+			for node in unvisited_nodes: # Iterate over the nodes
+				if current_min_node == None:
+					current_min_node = node
+				elif shortest_path[node] < shortest_path[current_min_node]:
+					current_min_node = node
+					
+			# The code block below retrieves the current node's neighbors and updates their distances
+			neighbors = graph.get_outgoing_edges(current_min_node)
+			
+			for neighbor in neighbors:
+				tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+				
+				#print(tentative_value)
+				
+				if tentative_value < shortest_path[neighbor]:
+					shortest_path[neighbor] = tentative_value
+					# We also update the best path to the current node
+					previous_nodes[neighbor] = current_min_node
+			
+			print(current_min_node)
+			unvisited_nodes.remove(current_min_node)
 		
-		print("Várias ligações:")
+
+		return previous_nodes, shortest_path
+
+
+
+
+
+
+
+
+
+	def CalculateShortestPath(self):
+		
+		
+		nodeId=[]
+
+		init_graph = defaultdict(dict)
+
+		for node in self.nodes:
+			nodeId.append(node.id)
+			init_graph[node.id] = {}
+
 		for node in self.nodes:
 			if node.online==1:
+				
 				for connections in node.connections:
 					#print("Adding connections")
 					#print(connections.fromNode)
 					#print(connections.toNode)
-					node_number1=connections.fromNode.replace("n", "")
-					node_number2=connections.toNode.replace("n", "")
-					
-					g.add_edge(int(node_number1) - 1, int(node_number2) - 1, 1)
+					node_number1=connections.fromNode
+					node_number2=connections.toNode
+					#print("Numero 1")
+					#print(node_number1)
 
-		D = self.dijkstra(g, 0)
-		print("Disjkstra: ")
-		print(D)
+					#print("Numero 2")
+					#print(node_number2)
+					init_graph[node_number1][node_number2] = 1
+			
+		
+		graph = Graph(nodeId, init_graph)
+
+		previous_nodes, shortest_path = self.dijkstra_algorithm(graph, "n1")
+
+		self.print_result(previous_nodes, shortest_path, "n1", "n2")
+
+
+
+	def print_result(self, previous_nodes, shortest_path, start_node, target_node):
+		path = []
+		node = target_node
+
+		while node != start_node:
+			path.append(node)
+			node = previous_nodes[node]
 	
+		# Add the start node manually
+		path.append(start_node)
+		
+		print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+		print(" -> ".join(reversed(path)))
+
 	def GetNetworkTopology(self):
 		file = minidom.parse('Topologia.xml')
 
@@ -168,34 +263,12 @@ class Servidor:
 		#		print(connection.fromIP)
 		
 
-	def dijkstra(self, graph, start_vertex):
-		#print("ENtrou no disjkstra")
-		D = {v:float('inf') for v in range(graph.v)}
-		D[start_vertex] = 0
-
-		pq = PriorityQueue()
-		pq.put((0, start_vertex))
-
-		while not pq.empty():
-			(dist, current_vertex) = pq.get()
-			graph.visited.append(current_vertex)
-
-			for neighbor in range(graph.v):
-				if graph.edges[current_vertex][neighbor] != -1:
-					distance = graph.edges[current_vertex][neighbor]
-					if neighbor not in graph.visited:
-						old_cost = D[neighbor]
-						new_cost = D[current_vertex] + distance
-
-						#Old_NOde e New Node e adicionar desta maneira
-						#DE forma a termos a rota para cada um dos nós
-
-						#old_cost = str(neighbor)
-						#new_cost = str(current_vertex) + str(distance)
-						if new_cost < old_cost:
-							pq.put((new_cost, neighbor))
-							D[neighbor] = new_cost
-		return D
+	def shortest(self, v, path):
+		"""make shortest path from v.previous"""
+		if v.previous:
+			path.append(v.previous.get_id())
+			self.shortest(v.previous, path)
+		return
 
 	def TopologyMaintenance(self):
 		"""Check what nodes still online"""
